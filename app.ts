5e171c4faa64f978f038e38e55a4dc6b81bedaeb
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-05-07 23:50:16 +0700

    Refactor task addition logic and update data.json structure to support multiple tasks

--- app.ts
+++ app.ts
@@ -1,21 +1,34 @@
 switch (process.argv[2]) {
     case "add":
-        const file = Bun.file("data.json");
-        file.write(JSON.stringify({
-            id: 1,
+        const filePath = "data.json";
+        let tasks: any[] = [];
+        try {
+            const raw = await Bun.file(filePath).text();
+            tasks = JSON.parse(raw);
+        } catch {
+            tasks = [];
+        }
+        const newTask ={
+            id: Math.random().toString(36).slice(2, 9),
             description: process.argv[3],
             status: "todo",
             createdAt: new Date().toISOString(),
             updatedAt: new Date().toISOString()
-        }));
+        };
+        tasks.push(newTask);
+        await Bun.write(filePath, JSON.stringify(tasks, null, 2));
         break;
+
     case "update":
         break;
+
     case "mark-in-progress":
         break;
+
     case "mark-done":
         break;
+
     case "list":
-        break;   
+        break;
 }
 export {};
